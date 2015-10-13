5f01ce8dab13976ef29de04f9f03a1c4c3572e75
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2015-10-13 19:57:33 +0000

    Creating branch for BioC 3.2 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_2/madman/Rpacks/DEXSeq@109591 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/core.R
@@ -0,0 +1,245 @@
+rmDepCols <- function(m)
+{
+    q <- qr(m)
+    if (q$rank < ncol(m)) 
+        m[, -q$pivot[(q$rank + 1):ncol(m)]]
+    else m
+}
+
+
+#BPPARAM <- MulticoreParam(workers=1)
+#fullModel <- design(object)
+#reducedModel <- ~ sample + exon
+vst <- function( x,  object ){
+    if( is.null( attr(object@dispersionFunction, "fitType") ) ) {
+        warnings("Dispersion function not found, applying log2(x+ 1) instead of vst...\n")
+        return( log10(x+1) )
+    }else if ( attr(object@dispersionFunction, "fitType") != "parametric" ){
+        warnings("Dispersion function not parametric, applying log2(x+ 1) instead of vst...\n")
+        return( log10(x+1) )
+    }
+  coefs <- attr(object@dispersionFunction, "coefficients")
+    (2/(sqrt(coefs["asymptDisp"]))) * log(2 * coefs["asymptDisp"] * 
+      sqrt(x) + 2 * sqrt(coefs["asymptDisp"] * (coefs["extraPois"] + 
+        1 + coefs["asymptDisp"] * x))) - (2/(sqrt(coefs["asymptDisp"]))) * 
+          log(2 * sqrt(coefs["asymptDisp"] * (coefs["extraPois"] + 1)))
+}
+
+
+testForDEU <-
+  function(object,
+           fullModel=design(object),
+           reducedModel= ~ sample + exon,
+           BPPARAM=MulticoreParam(workers=1))
+{
+  if (is.null(sizeFactors(object)) & is.null(normalizationFactors(object))) {
+    stop("first call estimateSizeFactors or provide a normalizationFactor matrix")
+  }
+  if (is.null(dispersions(object))) {
+    stop("first call estimateDispersions")
+  }
+
+  allVars <- all.vars(reducedModel)
+  if( any(!allVars %in% colnames( colData(object) )) ){
+     notPresent <- allVars[!allVars %in% colnames( colData(object) )]
+     notPresent <- paste(notPresent, collapse=",")
+     stop(sprintf("the variables '%s' of the parameter 'reducedModel' are not specified in the columns of colData", notPresent ) )
+  }
+
+  allVars <- all.vars(fullModel)
+  if( any(!allVars %in% colnames( colData(object) )) ){
+     notPresent <- allVars[!allVars %in% colnames( colData(object) )]
+     notPresent <- paste(notPresent, collapse=",")
+     stop(sprintf("the variables '%s' of the parameter 'fullModel' are not specified in the columns of colData", notPresent ) )
+  }
+
+  fullModelMatrix <- 
+    rmDepCols( model.matrix( fullModel, as.data.frame(colData(object)) ) )
+
+  reducedModelMatrix <- 
+    rmDepCols( model.matrix( reducedModel, as.data.frame(colData(object)) ) )
+
+  splitParts <- sort(
+    rep(seq_len(BPPARAM$workers), 
+    length.out=nrow(object) ) )
+  splitObject <- split( object, splitParts )
+
+  splitObject <- bplapply( splitObject,
+    function(x){
+      x <- nbinomLRT( x, reduced = reducedModelMatrix, full=fullModelMatrix )
+    }, BPPARAM=BPPARAM )
+
+  mergeObject <- do.call(rbind, splitObject)
+  matchedNames <- match( rownames(object), rownames(mergeObject))  
+  mcols(object) <- mcols( mergeObject )[matchedNames,]
+  assays(object) <- assays(mergeObject[matchedNames,])
+
+  extraAttributes <- setdiff( names( attributes(splitObject[[1]]) ),  names( attributes(object) ) )
+
+  for( atr in extraAttributes ){
+    attr( object, atr ) <- attr( splitObject[[1]], atr )
+  }
+
+  object
+ 
+}
+
+estimateExonFoldChanges <- function( object,
+                                    fitExpToVar = "condition",
+                                    denominator = "",
+                                    BPPARAM=MulticoreParam(workers=1), 
+                                    maxRowsMF=3000)
+{
+    stopifnot(is(object, "DEXSeqDataSet"))
+    # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+    # objects. Remove once all serialized DEXSeqDataSet objects around have
+    # been updated.
+    if (!.hasSlot(object, "rowRanges"))
+        object <- updateObject(object)
+    if (any(is.na(sizeFactors(object)))) {
+      stop("Please estimate sizeFactors first\n")
+    }
+    if (!fitExpToVar %in% colnames(sampleAnnotation(object))) {
+      stop(sprintf("The value of the parameter fitExpToVar,'%s', is not a column name of colData", fitExpToVar))
+    }
+    if ( is.null( dispersions(object) ) ){
+      stop("please call estimateDispersions first")
+    }
+    if ( length( attr( object, "test" ) ) == 0 ){
+      stop("please call testForDEU first")
+    }
+    frm <- as.formula(paste("count ~", fitExpToVar, "* exon"))
+    notNAs <- !is.na( results(object, 
+       filter=rowMeans( featureCounts(object, normalized=TRUE) ))$padj )
+    testablegenes <- unique(groupIDs(object)[notNAs])
+    groups <- groupIDs(object)
+    features <- featureIDs(object)
+    exonCounts <- featureCounts( object )
+    disps <- dispersions(object)
+    disps[is.na( disps )] <- 1e-8
+    mf <- object@modelFrameBM
+    rowsPerSample <- split(seq_len(nrow(mf)), mf$sample)
+    numsamples <- nrow( sampleAnnotation(object) )
+    features <- featureIDs(object)
+    countsAll <- featureCounts(object)
+    geteffects <- function(geneID){
+#        print( geneID )
+        rt <- groups %in% geneID
+        numexons <- sum(rt)
+        newMf <- mf[as.vector( sapply( split( seq_len(nrow(mf)), mf$sample ), "[", seq_len( numexons ) ) ),]
+        featuresInGene <- features[rt]
+        newMf$exon <- factor( rep( featuresInGene, numsamples ) )
+        countsThis <- countsAll[rt,]
+        rownames(countsThis) <- gsub("\\S+:", "", rownames(countsThis))
+        dispsThis <- disps[rt]
+        names(dispsThis) <- features[rt]
+        for( i in seq_len(nrow(newMf))){
+           newMf[i,"dispersion"] <- dispsThis[as.character(newMf[i,"exon"])]
+           newMf[i,"count"] <- countsThis[as.character(newMf[i,"exon"]), as.character(newMf[i,"sample"])]
+        }
+        newMf <- droplevels( newMf )
+        coefficients <- fitAndArrangeCoefs( frm, balanceExons = TRUE, mf=newMf, maxRowsMF=maxRowsMF)
+        if (is.null(coefficients)) {
+            return(coefficients)
+        }
+        ret <- t(getEffectsForPlotting(coefficients, averageOutExpression = TRUE, 
+            groupingVar = fitExpToVar))
+        rownames(ret) <- paste(geneID, rownames(ret), sep = ":")
+        return(ret)
+    }
+   
+    alleffects <- bplapply( testablegenes, geteffects, BPPARAM=BPPARAM )
+    alleffects <- do.call(rbind, alleffects)
+    alleffects <- vst(exp(alleffects), object)
+    toadd <- matrix(NA, nrow = nrow(object), ncol = ncol(alleffects))
+    rownames(toadd) <- rownames(object)
+    colnames(toadd) <- colnames(alleffects)
+    toadd[rownames(alleffects), colnames(alleffects)] <- alleffects
+    toadd <- DataFrame(toadd)
+    mcols(toadd) <- DataFrame(
+                                        type=rep("DEXSeq results",
+                                          ncol(toadd)),
+                                        description=rep("exon usage coefficient",
+                                          ncol(toadd) ) )
+    toadd2 <- matrix(NA, nrow = nrow(object), ncol = ncol(alleffects) )
+    if (denominator == "") {
+        denominator <- as.character(sampleAnnotation(object)[[fitExpToVar]][1])
+    }
+    colRemove <- colnames(alleffects) %in% denominator
+    stopifnot(any(colRemove))
+    denoCol <- which(colnames(alleffects) == denominator)
+    alleffects <- log2(alleffects/alleffects[, denoCol])
+    colnames(alleffects) <- sprintf("log2fold_%s_%s", colnames(alleffects), 
+            denominator)
+    colnames(toadd2) <- colnames(alleffects)
+    rownames(toadd2) <- rownames(object)
+    toadd2[rownames(alleffects), colnames(alleffects)] <- alleffects
+    toadd2 <- toadd2[,-denoCol, drop=FALSE]
+    toadd2 <- DataFrame(toadd2)
+    mcols(toadd2) <- DataFrame(type=rep("DEXSeq results",
+                                          ncol(toadd2)),
+                                       description=rep("relative exon usage fold change",
+                                          ncol(toadd2) ) )
+    allAdd <- cbind(toadd, toadd2)
+    mcols(object) <- cbind(mcols(object), allAdd)
+    object
+}
+
+
+DEXSeqResults <- function( object ){
+  stopifnot( is(object, "DEXSeqDataSet"))
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  LRTresults <- results(object, filter=rowMeans( featureCounts(object, normalized=TRUE) ) )
+  LRTresults$exonBaseMean <- rowMeans(featureCounts(object, normalized=TRUE))
+  LRTresults$featureID <- mcols(object)$featureID
+  LRTresults$groupID <- mcols(object)$groupID
+  LRTresults$dispersion <- mcols(object)$dispersion
+  
+  LRTresults <- LRTresults[,c("groupID", "featureID", "exonBaseMean", "dispersion", "stat", "pvalue", "padj")]
+  mcols( LRTresults )[colnames(LRTresults) %in% c("groupID", "featureID", "exonBaseMean"),"type"] <- "input"
+  mcols( LRTresults )[colnames(LRTresults) %in% "groupID","description"] <- "group/gene identifier"
+  mcols( LRTresults )[colnames(LRTresults) %in% "featureID","description"] <- "feature/exon identifier"
+  mcols( LRTresults )[colnames(LRTresults) %in% "exonBaseMean","description"] <- "mean of the counts across samples in each feature/exon"
+  mcols( LRTresults )[colnames(LRTresults) %in% "dispersion","description"] <- "exon dispersion estimate"
+  toadd <- mcols(object)[,mcols( mcols(object ) )$type == "DEXSeq results", drop=FALSE]
+  LRTresults <- cbind( LRTresults, toadd )
+  genomicData <- rowRanges(object)
+  mcols(genomicData) <- NULL
+  LRTresults$genomicData <- genomicData
+  LRTresults$countData <- featureCounts(object)
+  LRTresults$transcripts <- mcols(object)$transcripts
+  mcols( LRTresults )[colnames(LRTresults) %in% c("genomicData", "countData", "transcripts"),"type"] <- "input"
+  mcols( LRTresults )[colnames(LRTresults) %in% "genomicData","description"] <- "GRanges object of the coordinates of the exon/feature"
+  mcols( LRTresults )[colnames(LRTresults) %in% "countData","description"] <- "matrix of integer counts, of each column containing a sample"
+  mcols( LRTresults )[colnames(LRTresults) %in% "transcripts","description"] <- "list of transcripts overlapping with the exon"
+  dxr <-
+      new("DEXSeqResults",
+          LRTresults,
+          modelFrameBM=object@modelFrameBM,
+          sampleData = sampleAnnotation(object),
+          dispersionFunction = object@dispersionFunction )
+
+  dxr
+}
+
+DEXSeq <- function( object,
+                   fullModel=design(object),
+                   reducedModel = ~ sample + exon,
+                   BPPARAM=MulticoreParam(workers=1), fitExpToVar="condition", quiet=TRUE ){
+  stopifnot(is( object, "DEXSeqDataSet") )
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  object <- estimateSizeFactors( object )
+  object <- estimateDispersions( object, formula=fullModel, BPPARAM=BPPARAM, quiet=TRUE)
+  object <- testForDEU( object, reducedModel=reducedModel, fullModel=fullModel, BPPARAM=BPPARAM )
+  object <- estimateExonFoldChanges( object, fitExpToVar=fitExpToVar )
+  res <- DEXSeqResults( object )
+  res
+}
---
+++ R/methods.R
@@ -0,0 +1,223 @@
+
+estimateSizeFactors.DEXSeqDataSet <- function(object, locfunc=median, geoMeans) {
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  normFactors <- estimateSizeFactorsForMatrix(featureCounts(object), locfunc, geoMeans=geoMeans)
+  sizeFactors(object) <- rep(normFactors, 2)
+  maxExons <- length( unique( object@modelFrameBM$exon ) )
+  object@modelFrameBM$sizeFactor <- rep(normFactors, each=maxExons)
+  object
+}
+  
+setMethod("estimateSizeFactors", signature(object="DEXSeqDataSet"),
+          estimateSizeFactors.DEXSeqDataSet)
+
+
+estimateDispersions.DEXSeqDataSet <- 
+  function( object, fitType=c("parametric","local","mean"),
+    maxit=100, niter=10, quiet=FALSE, formula=design(object), BPPARAM=MulticoreParam(workers=1))
+{
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  if (is.null(sizeFactors(object)) & is.null(normalizationFactors(object))) {
+    stop("first call estimateSizeFactors or provide a normalizationFactor matrix before estimateDispersions")
+  }
+  if (!is.null(dispersions(object))) {
+    if (!quiet) message("you had estimated dispersions, replacing these")
+    mcols(object) <- mcols(object)[,!(mcols(mcols(object))$type %in% c("intermediate","results"))]
+  }
+  stopifnot(length(maxit)==1)
+  fitType <- match.arg(fitType, choices=c("parametric","local","mean"))
+
+  allVars <- all.vars(formula)
+  if( any(!allVars %in% colnames( colData(object) )) ){
+     notPresent <- allVars[!allVars %in% colnames( colData(object) )]
+     notPresent <- paste(notPresent, collapse=",")
+     stop(sprintf("the variables '%s' of the parameter 'formula' are not specified in the columns of the colData", notPresent ) )
+  }
+
+  splitParts <- sort(
+    rep(seq_len(BPPARAM$workers), 
+    length.out=nrow(object) ) )
+  splitObject <- split( object, splitParts )
+
+  modelMatrix <- rmDepCols(
+    model.matrix(formula, as.data.frame(colData(object))))
+  
+  splitObject <- bplapply( splitObject, 
+      function(x){
+        estimateDispersionsGeneEst(x, 
+          maxit=maxit, quiet=quiet, 
+          modelMatrix = modelMatrix, 
+          niter = niter)}, 
+    BPPARAM=BPPARAM )
+
+  mergeObject <- do.call( rbind, splitObject )
+  matchedNames <- match( rownames(object), rownames(mergeObject))  
+  mcols(object) <- mcols( mergeObject )[matchedNames,]
+  assays(object) <- assays(mergeObject[matchedNames,])
+
+  mcols(object)$baseMean <- rowMeans( featureCounts(object, normalized=TRUE) )
+  mcols(object)$baseVar <- mcols(object)$exonBaseVar
+  mcols(object)$allZero <- unname( rowSums( featureCounts(object)) == 0 |
+      rowSums(counts(object, normalized = TRUE)[, colData(object)$exon == "others"]) ==0 )
+
+  object <- estimateDispersionsFit(object, fitType=fitType, quiet=quiet)
+
+  dispPriorVar <- estimateDispersionsPriorVar(object, modelMatrix=modelMatrix)
+  
+  splitObject <- split( object, splitParts )
+  
+  splitObject <- bplapply( splitObject, 
+      function(x){
+        estimateDispersionsMAP(x, 
+          maxit=maxit, 
+          quiet=quiet, 
+          modelMatrix=modelMatrix, 
+          dispPriorVar=dispPriorVar)
+      }, 
+    BPPARAM=BPPARAM )
+
+  mergeObject <- do.call( rbind, splitObject ) 
+  matchedNames <- match( rownames(object), rownames(mergeObject) ) 
+  mcols(object) <- mcols( mergeObject )[matchedNames,]
+  mcols(object)$baseMean <- unname( rowMeans( counts(object, normalized=TRUE) ) )
+  mcols(object)$baseVar <- unname( rowVars( counts(object, normalized=TRUE) ) )
+  mcols(object)$dispersion <- pmin( mcols(object)$dispersion, ncol(object) )
+
+  object
+
+}
+
+setMethod( "estimateDispersions", signature(object="DEXSeqDataSet"),
+          estimateDispersions.DEXSeqDataSet )
+
+plotDispEsts.DEXSeqDataSet <- function(object, ymin,
+       genecol = "black", fitcol = "red", finalcol = "dodgerblue",
+       legend=TRUE, xlab, ylab, log = "xy", cex = 0.45, ...)
+{
+  stopifnot(is(object, "DEXSeqDataSet"))
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  mcols(object)$baseMean <- 
+    mcols(object)$exonBaseMean
+  mcols(object)$baseVar <- 
+    mcols(object)$exonBaseVar
+  plotDispEsts( as(object, "DESeqDataSet"), genecol=genecol, fitcol=fitcol,
+    finalcol=finalcol, legend=legend, log=log, cex=cex, ...)
+}
+
+setMethod( "plotDispEsts", signature(object="DEXSeqDataSet"),
+  plotDispEsts.DEXSeqDataSet )
+
+
+plotMA.DEXSeqDataSet <- function( object, alpha=0.1, ylim=c(-2, 2), foldChangeColumn=NULL, ...){
+  stopifnot( is(object, "DEXSeqDataSet") )
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  hasResults <- attr(object, "results")
+  dexseqResults <- mcols(object)[,mcols( mcols( object ) )$type == "DEXSeq results"]
+  if( ncol( dexseqResults ) == 0 ){
+    stop("first call estimateExonFoldChanges")
+  }
+  if( is.null(foldChangeColumn)){
+    y <- dexseqResults[,grep("log2fold", colnames(dexseqResults))[1]]
+  }else{
+    y <- dexseqResults[,foldChangeColumn]
+  }
+  x <- rowMeans( featureCounts(object, normalized=TRUE) )
+  df <- data.frame( x, y, results(object)$padj < alpha )
+#  ylim=c(-2, 2)
+  plotMA(df, ylim, ... )
+}
+
+setMethod("plotMA", signature(object="DEXSeqDataSet"),
+          plotMA.DEXSeqDataSet)
+
+plotMA.DEXSeqResults <- function(object, alpha=0.1, ylim=c(-2,2), foldChangeColumn=NULL, ...){
+  stopifnot( is(object, "DEXSeqResults") )
+  # Temporary hack for backward compatibility with "old" DEXSeqDataSet
+  # objects. Remove once all serialized DEXSeqDataSet objects around have
+  # been updated.
+  if (!.hasSlot(object, "rowRanges"))
+      object <- updateObject(object)
+  x <- rowMeans( counts(object, normalized=TRUE) )
+  dexseqResults <- object[,which( mcols( object )$type == "DEXSeq results" )]
+  if( is.null(foldChangeColumn)){
+    y <- dexseqResults[,grep("log2fold", colnames(dexseqResults))[1]]
+  }else{
+    y <- dexseqResults[,foldChangeColumn]
+  }
+  df <- data.frame( x, y, object$padj < alpha )
+#  ylim=c(-2, 2)
+  plotMA(df, ylim, ... )
+}
+
+setMethod("plotMA", signature(object="DEXSeqResults"),
+          plotMA.DEXSeqResults)
+    
+show.DEXSeqResults <- function(object){
+  cat("\n")
+  cat(mcols(object)[colnames(object) == "pvalue","description"])
+  cat("\n\n")
+  show(DataFrame(object))
+}
+
+setMethod("show", signature(object="DEXSeqResults"),
+          show.DEXSeqResults )
+
+counts.DEXSeqResults <- function(object, normalized=FALSE){
+    if( normalized ){
+        t(t( object$countData ) / object@sampleData$sizeFactor)
+    }else{
+       object$countData
+   }
+}
+
+setMethod( "counts", signature(object="DEXSeqResults"),
+          counts.DEXSeqResults )
+
+
+subsetByOverlaps.DEXSeqResults <- function( query, subject, maxgap = 0L, minoverlap = 1L,
+         type = c("any", "start", "end", "within", "equal"),
+         algorithm = c("nclist", "intervaltree"),
+         ignore.strand = FALSE ){
+  stopifnot( is( query, "DEXSeqResults") )
+  genomicData <- query$genomicData
+  overlaps <- findOverlaps( query=genomicData, subject=subject, maxgap=maxgap,
+    minoverlap=minoverlap, type=type,
+    algorithm=match.arg(algorithm), ignore.strand=ignore.strand )
+  query[queryHits( overlaps ),]
+}
+
+setMethod("subsetByOverlaps", signature(query="DEXSeqResults", subject="GenomicRanges"),
+          subsetByOverlaps.DEXSeqResults)
+
+findOverlaps.DEXSeqResults <- function( query, subject, maxgap = 0L, minoverlap = 1L,
+         type = c("any", "start", "end", "within", "equal"),
+         algorithm = c("nclist", "intervaltree"),
+         ignore.strand = FALSE ){
+  stopifnot( is( query, "DEXSeqResults") )
+  genomicData <- query$genomicData
+  overlaps <- findOverlaps( query=genomicData, subject=subject, maxgap=maxgap,
+    minoverlap=minoverlap, type=type,
+    algorithm=match.arg(algorithm), ignore.strand=ignore.strand)
+  overlaps
+}
+
+setMethod("findOverlaps", signature(query="DEXSeqResults", subject="GenomicRanges"),
+          findOverlaps.DEXSeqResults)
+
+
